b37046d677209ec3ef483a2b764e05a358d9e58e
Rohan Sharma <rohan921310@gmail.com>
2022-02-23 10:47:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client({ 
+    puppeteer: {
+        headless: true,
+	args: ['--no-sandbox','--disable-setuid-sandbox']
+    }
+});
+
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+    console.log('Client is ready!');
+});
+
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+});
+
+client.initialize();
