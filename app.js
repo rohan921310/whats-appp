7fe5bf8020646e4fc21278256ea521100f1ca580
Rohan Sharma <rohan921310@gmail.com>
2022-02-27 14:37:18 +0530

    final

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var app = express();
 
 const { Client } = require('whatsapp-web.js');
 var fs = require("fs");
-const client = new Client({ 
+const client = new Client({
     puppeteer: {
         headless: true,
-	args: ['--no-sandbox']
+        args: ['--no-sandbox']
     }
 });
 // https://medium.com/como-programar-em-1-dia/como-fazer-um-rob%C3%B4-de-whatsapp-chatbot-em-1-dia-644ee98054d7
@@ -20,48 +20,56 @@ const client = new Client({
 //   });
 
 
+function start() {
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-    console.log(qr);
-    console.log('d');
-});
+    client.on('qr', qr => {
+        qrcode.generate(qr, { small: true });
+        console.log(qr);
+        console.log('d');
+    });
+
+
+    client.on('ready', () => {
+        console.log('Client is ready!');
+
+        client.sendMessage('919213109261@c.us', 'hiii');
+    });
+
+    client.on('message', message => {
+        if (message.body === '!ping') {
+            message.reply('pong');
+        }
+    });
+
+
+    client.initialize();
+}
+
 
 
 app.get('/listUsers', function (req, res) {
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
     });
 
     client.sendMessage('919213109261@c.us', 'sendddd');
- })
-  
-//   server.listen(process.env.PORT || 80, () => {
-//     console.log("Listening on port 80");
-//   });
+})
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+    port = 3000;
 }
 app.listen(port, () => {
-  console.log(`Server successfully running on ${port}`);
+    console.log(`Server successfully running on ${port}`);
+});
+
+client.on('disconnected', (reason) => {
+    console.log('hj', reason)
+    start();
 });
 
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-    
-    client.sendMessage('919213109261@c.us', 'hiii');
-});
-
-client.on('message', message => {
-	if(message.body === '!ping') {
-		message.reply('pong');
-	}
-});
+start();
 
 
-
-client.initialize();
